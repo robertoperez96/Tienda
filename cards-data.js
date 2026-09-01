feb9340ce9704c8c3597bdc34a36dea8820a93d9
robertoperez96 <roberto.perezc96@gmail.com>
2026-08-31 20:49:29 -0600

    Add files via upload

--- cards-data.js
+++ cards-data.js
@@ -9,5 +9,8 @@
    TCGplayer verificado en esta conversación.
    ===================================================================== */
 window.CARDS = [
-  {id:1, name:"Team Rocket's Mewtwo ex", set:"MEGA Dream ex (M2a)", code:"M2a", num:237, total:193, rarity:"Special Art Rare", condition:"NM", language:"Japonés", type:"psiquico", kind:"single", price:2995, stock:1, isNew:true, image:"https://product-images.tcgplayer.com/fit-in/400x400/665908.jpg"}
+  {id:1, name:"Team Rocket's Mewtwo ex", set:"MEGA Dream ex (M2a)", code:"M2a", num:237, total:193, rarity:"Special Art Rare", condition:"NM", language:"Japonés", type:"psiquico", kind:"single", price:2995, stock:1, isNew:true, featured:true, image:"https://product-images.tcgplayer.com/fit-in/400x400/665908.jpg"},
+  {id:2, name:"Sandy Shocks ex", set:"SV: Prismatic Evolutions", code:"PRE", num:159, total:131, rarity:"Special Illustration Rare", condition:"NM", language:"Inglés", type:"metal", kind:"single", price:460, stock:1, isNew:true, featured:false, image:"https://product-images.tcgplayer.com/fit-in/400x400/610514.jpg"},
+  {id:3, name:"Gastly (ゴース) AR", set:"SV5K", code:"SV5K", num:80, total:71, rarity:"Art Rare", condition:"NM", language:"Japonés", type:"psiquico", kind:"single", price:680, stock:1, isNew:true, featured:false, image:""},
+  {id:4, name:"Shining Eevee (かがやくイーブイ)", set:"Pokémon GO (JP)", code:"SGO", num:53, total:71, rarity:"Art Rare", condition:"NM", language:"Japonés", type:"incoloro", kind:"single", price:300, stock:1, isNew:true, featured:false, image:""}
 ];
